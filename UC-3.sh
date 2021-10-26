f0d909e7f0e778137f78c511391993059d52ab9a
8948036201 <azharuddin.ansari108@gmail.com>
2021-10-26 16:54:05 +0530

    Adding UC-3.sh File

---
+++ UC-3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+echo "Enter the numbers"
+
+read -p "Enter 1st number : " a
+
+read -p "Enter 2nd number : " b
+
+read -p "Enter 3rd number : " c
+
+cal=$(($a+$b*$c))
+res=$(($a*$b+$c))
+echo $cal
+echo $res
