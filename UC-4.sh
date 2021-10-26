e320926f8c10309bcf93412c614149deeb516d1e
8948036201 <azharuddin.ansari108@gmail.com>
2021-10-26 16:54:33 +0530

    Adding UC-4.sh File

---
+++ UC-4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+echo "Enter the numbers"
+
+read -p "Enter 1st number : " a
+
+read -p "Enter 2nd number : " b
+
+read -p "Enter 3rd number : " c
+
+cal=$(($a+$b*$c))
+cal1=$(($a*$b+$c))
+cal2=$(($c+$a/$b))
+
+echo $cal
+echo $cal1
+echo $cal2
